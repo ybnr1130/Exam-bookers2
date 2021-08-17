145fd088fdfb79d28fdda16c15ad5ddf649ce30c
ybnr1130 <muuckk@gmail.com>
2021-08-17 06:04:35 +0000

    [Update]About画面追加

---
+++ bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def index
+    
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
+  
+end
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :books, only: [:create, :edit, :update, :index, :show, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+  get 'home/about' => 'homes#about' , as: 'about'
 end
