23c3f79abdc02131a6e43cc103cd681a91919541
ybnr1130 <muuckk@gmail.com>
2021-08-19 06:14:27 +0000

    [Update]User_Editからプロフィール画像投稿機能作成

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(user_params)
+    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully"
       redirect_to user_path(current_user)
--- bookers2/config/initializers/application_controller_renderer.rb
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7262a537354ac19b3b41bba4de5f42199b1cd0163b695ea3f30860dbe131027dbd670799f3dc785632df138f03847ce2d0cbc9c577f368d84e7b8580ca78ca10'
